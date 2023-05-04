b1b9a9563c2be0b07129597161748cb28dc01ad3
devartes <anacavalcante731@gmail.com>
2023-05-04 16:30:59 -0300

    feat: add Main and CompanyIntroduction

---
+++ components/Main/Main.tsx
@@ -0,0 +1,9 @@
+import CompanyIntroduction from "deco-sites/start/components/Main/CompanyIntroduction.tsx";
+
+export default function Main() {
+    return (
+      <main class="top-0 left-0 z-0 flex-center-between flex-col w-full">
+        <CompanyIntroduction img ="" heading_text="" text_bold="" />
+      </main>
+    );
+  }
