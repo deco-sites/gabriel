a1e97e435ede8acb2b4e52e6bc35f178493bd6f4
devartes <anacavalcante731@gmail.com>
2023-05-08 15:10:18 -0300

    feat(Main.tsx): add FirstSteps

--- components/Main/Main.tsx
+++ components/Main/Main.tsx
@@ -1,11 +1,13 @@
 import CompanyIntroduction from "deco-sites/start/components/Main/CompanyIntroduction.tsx";
 import Carousel from "deco-sites/start/components/Main/Carousel.tsx";
+import FirstSteps from "deco-sites/start/components/Main/FirstSteps.tsx";
 
 export default function Main() {
-    return (
-      <main class="top-0 left-0 z-0 flex-center-between flex-col w-full">
-        <CompanyIntroduction img ="" heading_text="" text_bold="" link={[]} />
-        <Carousel slider={[]} alt=""/>
-      </main>
-    );
-  }
+  return (
+    <main class="top-0 left-0 z-0 flex-center-between flex-col w-full">
+      <CompanyIntroduction img="" heading_text="" text_bold="" link={[]} />
+      <Carousel slider={[]} alt="" />
+      <FirstSteps title="" paragraph="" img="" alt="" cards={[]} />
+    </main>
+  );
+}
